5c77d7b525434adbf035ae1338719d7539c322f5
Anirban Singha <anirbansingha20@gmail.com>
2024-06-05 01:32:50 +0530

    Multer and cloudinary

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ dotenv.config({
   path: './env'
 })
 
-
-connect()
-
+connect().then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    })
+  }
+)
+.catch((error) => {
+  console.log("MONGO DB connection failed !!!", error);
+})
 
 
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY ,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+// use async await because this process will take time...
+const uploadonCloudinary = async(localFilePath) => {
+  try {
+      if (!localFilePath) return null
+      
+      const response = await cloudinary.uploader.upload(localFilePath, 
+        {
+          resource_type:"auto"
+        }
+      )
+      // uptil here file has been uploaded
+      console.log(response.url);
+      fs.unlinkSync(localFilePath)
+      return response
+
+  } catch (error) {
+      fs.unlinkSync(localFilePath)
+      return null
+  }
+}
+
+
+export {uploadonCloudinary}
