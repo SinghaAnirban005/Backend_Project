ab1b216cc10dc9b43d3af49b4ca3ba8cc22856b7
Anirban Singha <anirbansingha20@gmail.com>
2024-06-18 22:36:36 +0530

    Aggregation Pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -331,6 +331,91 @@ const updateAccountDetails = asyncHandler( async(req, res) => {
     )
 })
 
+  const getUserChannelProfile = asyncHandler((async (req , res) => {
+    const {username} = req.params
+
+    if(!username?.trim()) {
+      throw new ApiError(400, "username is misssing")
+    }
+    
+    // Writing aggregation pipelines ...
+
+    const channel = await User.aggregate([
+     { 
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+
+    {
+      // serach in document
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+
+      channelsSubscribedToCount: {
+        $size: "$subscribedTo"
+      },
+
+      isSubscribed: {
+        $cond: {
+          if: {$in: [req.user?._id, "subscribers.subscriber"]},
+          then: true,
+          else: false
+        }
+      }
+      }
+    },
+    // Deciding what to project or dispaly ...
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+      }
+    }
+    ])
+
+    if(!channel?.length) {
+      throw new ApiError(404, "channel does not exist")
+    }
+
+    // On returning aggregation pipelines we generally get an array of objects ..
+
+    return res
+    .status(200)
+    .json(
+      200,
+      channel[0],
+      "User channel fetched successfully"
+    )
+
+  }))
+
 
 const refreshAccessToken = asyncHandler( async (req, res) => {
 
@@ -393,5 +478,6 @@ export {
         updateAccountDetails,
         updateUserAvatar,
         updateUserCoverImage,
+        getUserChannelProfile
         
 }
