9fe8946cac10c7708316931a91aa4601e5f747fa
Anirban Singha <anirbansingha20@gmail.com>
2024-06-20 22:34:35 +0530

    Wrote subPipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -410,11 +410,68 @@ const updateAccountDetails = asyncHandler( async(req, res) => {
     .status(200)
     .json(
       200,
-      channel[0],
+      channel[0],    
       "User channel fetched successfully"
     )
 
   }))
+
+  const getWatchHistory = asyncHandler(async (req, res) => {
+    
+    const user = await User.aggregate([
+      {
+        $match: {
+          // creates a new instance of a Mongoose ObjectId. Mongoose ObjectIds are a special type used by MongoDB to uniquely identify documents
+          _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+      },
+
+      {
+        $lookup: {
+          from: "videos",
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+             {
+              $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                  {
+                    $project: {
+                      fullname: 1,
+                      username: 1,
+                      avatar: 1,
+                    }
+                  },
+                ]
+              }
+            }
+          ]
+        }
+      },
+
+      {
+        $addFields: {
+          owner: {
+            $first: "$owner" 
+          }
+        }
+      },
+
+    ])
+
+    return res
+    .status(200)
+    .json(
+      200,
+      user[0].watchHistory,
+      "Watch history fetched successfully"
+    )
+  })
 
 
 const refreshAccessToken = asyncHandler( async (req, res) => {
@@ -478,6 +535,7 @@ export {
         updateAccountDetails,
         updateUserAvatar,
         updateUserCoverImage,
-        getUserChannelProfile
+        getUserChannelProfile,
+        getWatchHistory
         
 }
